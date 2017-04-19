9994e14017fc91574363eef530de20561d5b0291
Julian Chan <julian.chan@mail.com>
2017-04-20 00:00:00 +1000

    moved score calculation to server; hide vertical gridlines on chart

--- public/chart.js
+++ public/chart.js
@@ -1,9 +1,10 @@
 Chart.defaults.bar.scales.xAxes[0].categoryPercentage = 1;
 Chart.defaults.bar.scales.xAxes[0].barPercentage = 1.33;
+Chart.defaults.bar.scales.xAxes[0].gridLines.display = false;
 Chart.defaults.scale.ticks.beginAtZero = true;
 Chart.defaults.global.legend.display = false;
 
-var $chart = $('.chart');
+var chart, $chart = $('.chart');
 var TARGET_SCORE = 1000;
 
 var backgroundColors = [
@@ -49,40 +50,31 @@ var options = {
 }
 
 var drawChart = function(teams) {
-  var teamNames = teams.map(function(team) { return team.name; });
   var teamScores = teams.map(function(team) { return team.score; });
-  var backgroundColor = teams.map(function(team, index) { return backgroundColors[index % backgroundColors.length]; });
-  var borderColor = teams.map(function(team, index) { return borderColors[index % borderColors.length]; });
 
-  var data = {
-    labels: teamNames,
-    datasets: [{
-      label: 'Current score',
-      data: teamScores.slice(),
-      backgroundColor: backgroundColor,
-      borderWidth: 1
-    }]
-  }
+  if (chart) {
+    chart.data.datasets[0].data = teamScores;
+    chart.update();
+  } else {
+    var teamNames = teams.map(function(team) { return team.name; });
+    var backgroundColor = teams.map(function(team, index) { return backgroundColors[index % backgroundColors.length]; });
+    var borderColor = teams.map(function(team, index) { return borderColors[index % borderColors.length]; });
 
-  var chart = new Chart($chart, {
-    type: 'bar',
-    data: data,
-    options: options
-  });
-
-  setInterval(function() {
-    var currentScores = chart.data.datasets[0].data;
-    var scoresBelowTarget = currentScores.filter(function(score) { return score < TARGET_SCORE; });
-
-    if (scoresBelowTarget.length === 0) {
-      chart.data.datasets[0].data = teamScores.slice();
-    } else {
-      chart.data.datasets[0].data.forEach(function(value, index) {
-        var random = Math.floor(Math.random() * 81 + 20) + 400 / Math.sqrt(chart.data.datasets[0].data[index]);
-        chart.data.datasets[0].data[index] += random;
-      });
+    var data = {
+      labels: teamNames,
+      datasets: [{
+        label: 'Current score',
+        data: teamScores,
+        backgroundColor: backgroundColor,
+        borderColor: borderColor,
+        borderWidth: 1
+      }]
     }
 
-    chart.update();
-  }, 2000);
+    chart = new Chart($chart, {
+      type: 'bar',
+      data: data,
+      options: options
+    });
+  }
 }
--- public/main.js
+++ public/main.js
@@ -1,5 +1,11 @@
-$.ajax({
-  url: '/api/teams'
-}).done(function(teams) {
-  drawChart(teams);
-});
+var displayScores = function() {
+  $.ajax({
+    url: '/api/teams'
+  }).done(function(teams) {
+    drawChart(teams);
+  });
+};
+
+displayScores();
+
+setInterval(displayScores, 10000);
