23f82a8f34cc9e63205796341073cb3b506bd0cd
will <mail@wilmaguire.com>
2017-04-24 10:41:08 +1000

    -css_update

--- public/js/main.js
+++ public/js/main.js
@@ -24,15 +24,12 @@ var displayTweets = function() {
 };
 
 displayScores();
-// displayTweets();
+displayTweets();
 
 setInterval(displayScores, 5000);
-<<<<<<< HEAD
-setInterval(displayTweets, 5000);
-=======
-// setInterval(displayTweets, 5000);
+
 var $scrollingDiv = $(".soc_media");
->>>>>>> WorkingBranch
+
 
 $(window).scroll(function(){
        $scrollingDiv
